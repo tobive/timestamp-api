8098abffbe87060324b55cb56922754eecd21b12
Tobi <knightsofkangouw@gmail.com>
2017-02-09 06:24:26 +0000

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express')
 var app = express()
+var moment = require('moment')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:id',function(req,res){
+  console.log(new Date(), req.method, req.url);
+  var objDate = {"unix": null, "natural": null}
+  //if(err) throw err
+  if((/^-?[0-9]+$/).test(req.params.id)){ //req in UNIX time
+    console.log("req.params.id unix time")
+    objDate.unix = parseInt(req.params.id)
+    objDate.natural = moment(parseInt(req.params.id)).format("MMMM DD, YYYY")
+  } else {
+    console.log("req.params.id natural language")
+    if(moment(req.params.id).isValid()){
+      objDate.unix = parseInt(moment(req.params.id).format("X"))
+      objDate.natural = moment(req.params.id).format("MMMM DD, YYYY")
+    }
+  }
+  res.send(objDate)
+  
 })
 
-app.listen(process.env.PORT||8080, function () {
-  console.log('Example app listening on port!')
+//process.env.PORT is the one used in heroku
+app.listen(process.env.PORT||$PORT, function () {
+  console.log('timestamp microservice is listening on port!')
 })
